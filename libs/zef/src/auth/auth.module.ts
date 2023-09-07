a36e64595ccac91abf06020a5e2258391bb69b2b
Aleš <foxxcz@gmail.com>
2023-09-07 16:32:04 +0200

    refreshTokeKey configurable

--- libs/zef/src/auth/auth.module.ts
+++ libs/zef/src/auth/auth.module.ts
@@ -31,6 +31,10 @@ export function defaultTokenGetter(response: any) {
 @NgModule({})
 export class ZefAuthModule {
   static forRoot(config?: AuthModuleConfig): ModuleWithProviders<ZefAuthRootModule> {
+    console.log(config, config && config.refreshTokenKey
+      ? config.refreshTokenKey
+      : 'refreshTokenId');
+
     return {
       ngModule: ZefAuthRootModule,
       providers: [
